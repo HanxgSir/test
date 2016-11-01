342014d2ef995146b8e76c6ac35df09852edc7b6
HanxgSir <18304018283@163.com>
2016-11-01 11:23:58 +0800

    add some services but one of the services can not run

--- musicPlayer/app.py
+++ musicPlayer/app.py
@@ -7,14 +7,35 @@ app = Flask(__name__)
 @app.route('/')
 def create_app():
     songs = {
+        "area": [
+            {"name": u"欧美", "id": 1},
+            {"name": u"大陆", "id": 2},
+            {"name": u"日韩", "id": 3},
+            {"name": u"港台", "id": 4}
+        ],
         "type": [
-            u"欧美",
-            u"大陆",
-            u"日韩",
-            u"港台"
+            {"name": u"抒情", "id": 1},
+            {"name": u"舞曲", "id": 2},
+            {"name": u"怀旧", "id": 3},
+            {"name": u"摇滚", "id": 4}
         ]
     }
     return json.dumps(songs)
+
+
+@app.route('/area/<int:area_id>')
+def get_area_songs(area_id):
+    if area_id == 1:
+        return jsonify(status=0, songs={
+            "text": "Any Man Of Mine - Shania Twain",
+            "src": "../songs/Any Man Of Mine - Shania Twain.mp3"
+        })
+    elif area_id == 2:
+        return jsonify(status=0, the_area="http://music.baidu.com/tag/%E5%9B%BD%E8%AF%AD")
+    elif area_id == 3:
+        return jsonify(status=0, the_area="http://music.baidu.com/tag/%E9%9F%A9%E8%AF%AD")
+    else:
+        return jsonify(status=0, the_area="http://music.baidu.com/tag/%E7%B2%A4%E8%AF%AD")
 
 
 if __name__ == '__main__':
--- musicPlayer/pages/player/player.js
+++ musicPlayer/pages/player/player.js
@@ -2,7 +2,9 @@
 
 var app = getApp();
 Page({
-    data: {},
+    data: {
+        songs:""
+    },
     onLoad: function () {
         console.log('load');
         var that = this;
@@ -13,8 +15,45 @@ Page({
                 console.log('success');
                 console.log(data);
                 that.setData({
+                    area: data.data.area,
                     type: data.data.type
                 })
+            }
+        })
+    },
+    area_click: function (event) {
+        console.log(event);
+        var that = this;
+        wx.request({
+            url: 'http://127.0.0.1:5000/area/' + event.target.id,
+            method: 'get',
+            success: function (data) {
+                console.log(data);
+                that.setData({
+                    songs: {
+                        text:data.data.songs.text,
+                        src: data.data.songs.src
+                    }
+                })
+            }
+        })
+    },
+    play_songs: function () {
+        console.log(this.data.songs.src);
+        var that = this;
+        wx.getBackgroundAudioPlayerState({
+            success: function (res) {
+                var status = res.status;
+                var dataUrl = res.dataUrl;
+                var currentPosition = res.currentPosition;
+                var duration = res.duration;
+                var downloadPercent = res.downloadPercent;
+            }
+        });
+        wx.playBackgroundAudio({
+            dataUrl: that.data.songs.src,
+            success: function(res){
+                console.log(res);
             }
         })
     }
